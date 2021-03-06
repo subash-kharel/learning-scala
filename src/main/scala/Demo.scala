48dc9aa79cb47d39caea5699919aed54ec3bf88d
Subash Kharel <subashkharel@Subashs-MBP.attlocal.net>
2021-03-06 07:36:21 -0600

    if else

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,30 +1,19 @@
 object Demo {
 
   def main(args: Array[String]): Unit ={
-    println("Hello World!")
-    val name = "subash"
-    val age = 31
-    //String interpolation using + (similar to java)
-    println(name + " is " + age + " years old")
-    //String interpolation using s (all dynamic variable are denoted by $)
-    //This is called simple string interpolation
-    println(s"$name is $age years old")
-    //String interpolation making the values type safe
-    //this would fail if lets say age was 31.2 or any other decimal because %d is ensuring that the value is integer
-    //and %s is ensuring that the variable is of type string
-    //This is also called formatted string interpolation
-    println(f"$name%s is $age%d years old")
+    val x = 50
+    val y =10;
+    if(x == 50){
+      println(s"The value of x is $x")
+    } else {
+      println(s"The value of x is not 50")
+    }
 
-    //Simple string interpolation with escape character
-    //this will print hello and world in different line as such:
-    //hello
-    //world
-    println(s"Hello \nworld")
-
-    //This is an example raw string interpolation
-    //this will print hello \nworld in one line and will skip the \n because it prints the raw object.
-    println(raw"hello \nworld")
-
+    //this is if expression
+    val result = if(y == 10) "y == 10" else "y != 10"
+    //or
+    //println(if(y == 10) "y == 10" else "y != 10")
+    println(result);
 
   }
 
