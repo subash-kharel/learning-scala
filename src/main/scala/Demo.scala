d7c7e60edebe7df9aa98c022beb93c60e583a592
Subash Kharel <subashkharel@Subashs-MBP.attlocal.net>
2021-03-06 08:36:49 -0600

    while, do while, for loop and case statements

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,31 +1,106 @@
 object Demo {
 
+  //****NOte*****: there are println() few places just to print new lines for cleaner output.
   def main(args: Array[String]): Unit ={
-    println("Hello World!")
-    val name = "subash"
-    val age = 31
-    //String interpolation using + (similar to java)
-    println(name + " is " + age + " years old")
-    //String interpolation using s (all dynamic variable are denoted by $)
-    //This is called simple string interpolation
-    println(s"$name is $age years old")
-    //String interpolation making the values type safe
-    //this would fail if lets say age was 31.2 or any other decimal because %d is ensuring that the value is integer
-    //and %s is ensuring that the variable is of type string
-    //This is also called formatted string interpolation
-    println(f"$name%s is $age%d years old")
 
-    //Simple string interpolation with escape character
-    //this will print hello and world in different line as such:
-    //hello
-    //world
-    println(s"Hello \nworld")
+    println()
+    var x = 0
+    while( x < 10){
+      print(s"$x ")
+      //cannot write x++ or x--;
+      x+=1
+    }
+    println()
 
-    //This is an example raw string interpolation
-    //this will print hello \nworld in one line and will skip the \n because it prints the raw object.
-    println(raw"hello \nworld")
+    var y = 0
 
+    do{
+      print(s"$y ")
+      y += 1
+    } while(y < 10)
+  }
+
+  println()
+  // dont need to do var i because scala automatically makes i a var which is mutable.
+//  for( i <- 1 to 5){
+//    print(s"$i ")
+//  }
+
+  //this is same as above, to is a method that take one parameter
+  for( i <- 1.to(5)){
+    print(s"$i ")
+  }
+
+  println()
+
+  //this also gives same result as above. So to and until are methods that gives us range.
+  // the arrow is called generator
+  for( i <- 1 until 6){
+    print(s"$i ")
+  }
+
+  println()
+  //nester for loops
+  for(i <- 1 to 5; j <- 1 to 3){
+    println(s"$i $j")
+  }
+
+  println()
+
+  // creating a list and iterating over a list
+  var list = List(1,2,3,4,5,6,7,8,9);
+  for(i <- list){
+    println(i)
+  }
+
+  println()
+
+  // for loop with filters, this will onely print out 1,2,3,4,5
+  for(i <- list; if i <6){
+    println(i)
+  }
+
+  println()
+  //using for loop as an expression
+
+  //this should return the list of result and should look like this: List(1, 4, 9, 16, 25)
+  var result = for { i <-list; if i<6} yield {
+    i*i
+  }
+  println(result)
+
+  //match as statement
+
+  var age = 20
+
+  age match {
+    case 20 => println("age is "+age);
+    case 30 => println("age is "+age);
+    case 40 => println("age is "+age);
+    case 50 => println("age is "+age);
+    case 60 => println("age is "+age);
+      //this last case is how to define default case
+    case _ => println("default")
 
   }
 
+  //match as expression
+
+   var result1 = age match {
+    case 20 => age
+    case 30 => age
+    case 40 => age
+    case 50 => age
+    case 60 => age
+    case _ => println("default")
+  }
+  println(s"The age with match expression is $result1")
+
+  //case statement where multiple checks could return same result
+  var i = 7;
+
+  i match {
+    case 1|3|5|7|9 => println("odd")
+    case 2|4|6|8|10 => println("even")
+  }
 }
