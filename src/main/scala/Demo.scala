8e22c6e7578c9e256fc429511098760d558a7148
Subash Kharel <subashkharel@Subashs-MBP.attlocal.net>
2021-03-08 11:34:37 -0600

    learned basic scala funtions

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,31 +1,40 @@
 object Demo {
 
+  def add(x : Int, y : Int): Int = {
+    return x +y
+  }
+
+  def subtract(x : Int, y : Int): Int = {
+
+    //return is optional in scala if the last line is the return.
+     x - y
+  }
+
+  //return is also optional here.
+  def multiply(x : Int, y : Int): Int = return x * y
+
+  //if we are certain that the divide will always return integer then we can remove te return type as well
+  def divide(x : Int, y : Int) =  x / y
+
+  object Math {
+    def add(x : Int, y : Int): Int = {
+      return x +y
+    }
+
+    def square(x: Int) = x *x
+  }
+
   def main(args: Array[String]): Unit ={
-    println("Hello World!")
-    val name = "subash"
-    val age = 31
-    //String interpolation using + (similar to java)
-    println(name + " is " + age + " years old")
-    //String interpolation using s (all dynamic variable are denoted by $)
-    //This is called simple string interpolation
-    println(s"$name is $age years old")
-    //String interpolation making the values type safe
-    //this would fail if lets say age was 31.2 or any other decimal because %d is ensuring that the value is integer
-    //and %s is ensuring that the variable is of type string
-    //This is also called formatted string interpolation
-    println(f"$name%s is $age%d years old")
 
-    //Simple string interpolation with escape character
-    //this will print hello and world in different line as such:
-    //hello
-    //world
-    println(s"Hello \nworld")
-
-    //This is an example raw string interpolation
-    //this will print hello \nworld in one line and will skip the \n because it prints the raw object.
-    println(raw"hello \nworld")
-
-
+    println(add(3,2))
+    println(subtract(3,2))
+    println(multiply(3,2))
+    println(divide(3,2))
+    println(Math.add(4,5))
+    //both ways to call square are valid
+    println(Math.square(3))
+    //this only works when function takes only one argument
+    println(Math square 3)
   }
 
 }
