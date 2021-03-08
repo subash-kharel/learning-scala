05775d227862623af31f8dae232189c8084a4f41
Subash Kharel <subashkharel@Subashs-MBP.attlocal.net>
2021-03-08 14:41:16 -0600

    examples of higher order function

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,47 +1,31 @@
+//HIgher order functions are those functions which are abel to take functions as arguments and able to
+//return function as a result
+
 object Demo {
 
-  object Math {
-    def add(x : Int = 45, y : Int = 15): Int = {
-      return x +y
-    }
+  def math( x:Double, y:Double,functionName: (Double, Double)=> Double): Double = functionName(x,y)
 
-    //could use operaters in scala as function name.
-    def +(x : Int = 45, y : Int = 15): Int = {
-      return x +y
-    }
-
-    def square(x: Int) = x *x
-
-  }
+  def math( x:Double, y:Double, z: Double, functionName: (Double, Double)=> Double): Double = functionName(functionName(x,y),z)
 
 
-  // return type Unit is like a void in java
-  def printExample(x: Int, y: Int): Unit = {
-    println(x+y)
-  }
 
-  def main(args: Array[String]): Unit ={
-    //This will word even though the method is expection 2 params because we have set the default values for the
-    //parameters when the values are not provided.
-      println(Math.add())
+  def main(args: Array[String]): Unit = {
+    //this result constant takes two double values and the function to add as a parament
+     val result = math(50, 20, (x,y)=>x+y)
+    //this should pring 70
+     println(result)
 
-    //this we get result of 25 because this function will take 10 as the first parameter
-      println(Math.add(10))
+    val multiply = math(2,3, (x,y) => x*y)
+    //this should pring 6.0
+    println(multiply)
 
-    //this will return 20
-      println(Math.add(10, 10))
+    //this will add the first two params first and the invokes the same function to add result to the third paramenter
+    val addWithThreeArguments = math(1,2,3,(x,y)=> x+y)
+    println(addWithThreeArguments)
 
-    //this will print result of x+y
-     printExample(1,2)
+    // using wildcard _ also does the same thing like the function above
+    val addWithThreeArguments1 = math(1,2,3, _+_)
+    println(addWithThreeArguments)
 
-
-    //This will print the sum of 1 and 4 and + is not the operator here but the funtionName.
-    println( Math.+(1,4))
-
-    //this is an example of anonymous function
-    var add = (x:Int , y:Int)=>x+y
-    println("THis is an example of anonymous function "+ add(1,3))
-
-  }
-
+}
 }
