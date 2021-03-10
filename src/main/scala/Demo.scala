0f6054d6bc57fc5b53a0eac37bdf95afd1595cf4
Subash Kharel <subashkharel@Subashs-MBP.attlocal.net>
2021-03-10 13:14:13 -0600

    learned flatten, flatmap, map and filter

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,43 +1,44 @@
-//Scala - Options ( options is a container that give you two value, weather it give instance of some or none.
+//Scala - map, filter
+
 
 object Demo {
 
-  val lst = List(1,2,3)
-  val map = Map(1 ->"Tom", 2 -> "Max", 3 ->"John")
+  val lst = List(1,2,3,4,5,6,10,13,15)
 
-  //creating option and initializing empty.
-  val opt: Option[Int] = None;
+  val myMap = Map(1 ->"Subash", 2->"Minu", 3 -> "Aurora", 4 -> "Arish")
 
-  val opt1: Option[Int] = Some(10)
   def main(args: Array[String]): Unit = {
 
-    //this prints out None
-   println( lst.find(_ > 6))
+    //this will return the double of each number in the List lst.
+    println(lst.map(_*2))
 
-    //this will return 3
-    println(lst.find(_ > 2).get)
+    //this is similar to how java does it and does the same thing as above
+    println(lst.map(x => x *2))
 
-    //this prints out Some(2)
-    //options can give you two kind of result some or none.
-    //find has a return type option
-    println( lst.find(_ > 2))
+    //this will print: List(hi(1,Subash), hi(2,Minu), hi(3,Aurora), hi(4,Arish))
+    println(myMap.map(x => "hi" +x))
 
-    //way to handle when it cannot find
-    println(lst.find(_>6).getOrElse(0))
+    //this will print: Map(1 -> Subash, 2 -> Minu, 3 -> Aurora, 4 -> Arish)
+    println(myMap.view.mapValues(x =>x).toMap)
 
-    //this returns some(Tom)
-    println(map.get(1))
+    //coverting a string to uppercase
+    println("hello".map(_.toUpper))
+    //printing list of list: List(List(1, 2, 3), List(3, 4, 5, 6))
+    println(List(List(1,2,3), List(3,4,5,6)))
 
-    //this will give you the value just like in java i.e. option.get
-    println(map.get(1).get)
+    //this flattens the both list: List(1, 2, 3, 3, 4, 5, 6)
+    println(List(List(1,2,3), List(3,4,5,6)).flatten)
 
-    //with map.get(5) it will throw exception because there is no 5 so we need to do getorElse
-    println(map.get(5).getOrElse("No name found"))
+    //this will iterate over every element in the list and adds one to every element and return list of list
+    //this will return:
+    //List(List(1, 2), List(2, 3), List(3, 4), List(4, 5), List(5, 6), List(6, 7), List(10, 11), List(13, 14), List(15, 16))
+    println(lst.map(x => List(x, x+1)))
 
-    //this should return true
-    println(opt.isEmpty)
+    //this will return:List(1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 10, 11, 13, 14, 15, 16)
+    println(lst.flatMap(x => List(x, x+1)))
 
-    //this should return false because opt1 is not empty
-    println(opt1.isEmpty)
+    //this will filter through the list and return anything greater then a 5
+    //The result looks like this: List(6, 10, 13, 15)
+    println(lst.filter(x =>x >5))
   }
 }
