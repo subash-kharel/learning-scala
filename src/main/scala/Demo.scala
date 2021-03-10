aba829733921d7a84c725e3b75260b8565723b4d
Subash Kharel <subashkharel@Subashs-MBP.attlocal.net>
2021-03-10 13:48:56 -0600

    learned basics of scala classes

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,41 +1,40 @@
-//Scala - reduce, fold or scan (Left/Right)
-//reduceLeft, reduceRight, foldLeft, foldRight, scanLeft, scanRight
+//Scala - Class
+
+//this is how you define class
+
+//class User;
+
+//creating class with constructer
+//need to add var infront
+//can add access modifier
+
+//class User(var name: String, private var age: Int)
 
 
+//you can add curly braces to add a method to a class to acess private values
+class User(var name: String, private var age: Int){
+
+  def printAge(age: Int): Unit = {
+    println(s"The age is $age")
+  }
+}
+
+
+
+
+//object means that this is singleton class we cannot create instance out of this object
 object Demo {
 
-  val lst = List(1,2,3,5,7,10,13)
+  def main(args:Array[String]): Unit ={
+    //creating an instance of class
+    var user = new User("Max", 28)
+    //this will print Max
+    println(user.name)
 
-  val lst2 = List("A", "B", "C")
+    //overwriting name
+    user.name = "subash"
+    println(user.name)
 
-
-  def main(args: Array[String]): Unit = {
-
-    //first two item in list is added and again the result is added to the next item in the list
-    //for this result is: ABC
-    println(lst2.reduceLeft(_ + _))
-
-    //the result here is: 41
-    println(lst.reduceLeft(_ + _))
-
-    //this is same as above
-    println(lst.reduceLeft((x,y)=> x+y))
-
-    //this shows the same result as above becase it is a
-    //this will iterate the last two values and uses that result to do further processing to the next operation
-    println(lst.reduceRight(_ +_))
-
-
-    //this will add 5 to the total sum so result should be 41 +5  = 46
-    // so 5 +1 = 6 +2 = 8 +........
-    println(lst.foldLeft(5)(_ +_))
-    //this prints ZABC
-    println(lst2.foldLeft("Z")(_ + _))
-    ///gives you all intermediate values
-    //This is what the result looks like: List(100, 101, 103, 106, 111, 118, 128, 141)
-    println(lst.scanLeft(100)(_ + _))
-
-    //this is the result: List(z, zA, zAB, zABC)
-    println(lst2.scanLeft("z")(_ +_))
+    user.printAge(25)
   }
 }
