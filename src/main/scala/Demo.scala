79973c565fd3c7fc76c1f9bcf18b11958c232822
Subash Kharel <subashkharel@Subashs-MBP.attlocal.net>
2021-03-25 12:37:48 -0500

    dynamic arrays and multi dimentional arrays

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -9,7 +9,8 @@ object Demo {
   val myArray2 = new Array[Int](5)
 
   //another way of initializing an array
- val myArray3 = Array(1,2,3,4,5,6)
+  val myArray3 = Array(1, 2, 3, 4, 5, 6)
+
   def main(args: Array[String]): Unit = {
 
     //assigning valaue to an array
@@ -21,15 +22,45 @@ object Demo {
     //this is just print the object reference which is why we needed the for loop
     println(myArray)
 
-    for(x <- 0 to (myArray.length -1)){
+    for (x <- 0 to (myArray.length - 1)) {
       println(x)
     }
 
-    val concatinatedArray = Array.concat(myArray,myArray2);
-    println ("concatination starts here---->")
-     for(i <-concatinatedArray){
+    val concatinatedArray = Array.concat(myArray, myArray2);
+    println("concatination starts here---->")
+    for (i <- concatinatedArray) {
 
-       println(i)
-     }
-}
+      println(i)
+    }
+
+
+    //multi dimensional arrays
+
+    //we need to import Array ._ to get all arrays libraries it is like * in java
+    import Array._
+    //this creates 2 /3 matrix
+    var multiDim = ofDim[Int](2, 3)
+    multiDim(1)(2) = 5
+    for (i <- multiDim) {
+      //this will print 000 and 005
+      println(i.mkString)
+    }
+
+    //another way to print the string representation of the arrrays
+    print(multiDim.map(_.mkString).mkString("\n"))
+  }
+
+
+  //Dynamic arrays - of variable size
+  var arrays1 = scala.collection.mutable.Buffer[String]()
+  arrays1 += "Ben"
+  arrays1 += "Chrish"
+
+  //arrays1 is an array buffer and we can increase the size of array as needed
+  println(arrays1)
+  //converting to array
+  arrays1.toArray
+  //convertin to list
+  arrays1.toList
+
 }
