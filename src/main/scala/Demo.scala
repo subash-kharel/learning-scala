0ffcc4fd4add62cea72b37a64b01139b203bc58d
Subash Kharel <subashkharel@Subashs-MBP.attlocal.net>
2021-03-25 13:13:59 -0500

    reading and writing to and from the file

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -2,65 +2,37 @@
 
 object Demo {
 
-  //declaring an array
-  val myArray: Array[Int] = new Array[Int](4)
-
-  //another way of declaring an array
-  val myArray2 = new Array[Int](5)
-
-  //another way of initializing an array
-  val myArray3 = Array(1, 2, 3, 4, 5, 6)
 
   def main(args: Array[String]): Unit = {
 
-    //assigning valaue to an array
-    myArray(0) = 1
-    myArray(1) = 2
-    myArray(2) = 3
-    myArray(3) = 4
+    import scala.io.Source
 
-    //this is just print the object reference which is why we needed the for loop
-    println(myArray)
+    //this is will print out the current user directory
+    println("Current directory: " + System.getProperty("user.dir"))
 
-    for (x <- 0 to (myArray.length - 1)) {
-      println(x)
+    val dataFile = "/Users/subashkharel/Documents/learning-scala/datafile.txt"
+
+    //Source create from file
+    val fileData = Source.fromFile(dataFile).getLines()
+    //find number of lines with hello on it
+    var helloCount = 0;
+    for( line <- fileData){
+      if( line.indexOf("hello") >= 0){
+        helloCount += 1
+      }
     }
-
-    val concatinatedArray = Array.concat(myArray, myArray2);
-    println("concatination starts here---->")
-    for (i <- concatinatedArray) {
-
-      println(i)
-    }
+    println("total hello's---> "+ helloCount)
 
 
-    //multi dimensional arrays
 
-    //we need to import Array ._ to get all arrays libraries it is like * in java
-    import Array._
-    //this creates 2 /3 matrix
-    var multiDim = ofDim[Int](2, 3)
-    multiDim(1)(2) = 5
-    for (i <- multiDim) {
-      //this will print 000 and 005
-      println(i.mkString)
-    }
 
-    //another way to print the string representation of the arrrays
-    print(multiDim.map(_.mkString).mkString("\n"))
+
+    //save to a file
+    import java.io._
+    //this creates a new file demo.log
+    val writer = new PrintWriter(new File("demo.log"))
+    //this writes the value to the file
+    writer.write(helloCount)
+    writer.close()
   }
-
-
-  //Dynamic arrays - of variable size
-  var arrays1 = scala.collection.mutable.Buffer[String]()
-  arrays1 += "Ben"
-  arrays1 += "Chrish"
-
-  //arrays1 is an array buffer and we can increase the size of array as needed
-  println(arrays1)
-  //converting to array
-  arrays1.toArray
-  //convertin to list
-  arrays1.toList
-
 }
